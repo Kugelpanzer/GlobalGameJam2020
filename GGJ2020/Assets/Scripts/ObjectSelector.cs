d67a2235aeb1e26b7dff174b28661e4226c3d5f1
Kugelpanzer <djolemlad@gmail.com>
2020-02-01 17:12:26 +0100

    change collider collor prep

--- GGJ2020/Assets/Scripts/ObjectSelector.cs
+++ GGJ2020/Assets/Scripts/ObjectSelector.cs
@@ -6,6 +6,15 @@ public class ObjectSelector : MonoBehaviour
 {
 
     public GameObject selectedTile;
+
+    public BaseObject checkingObject;
+    public int allowedAsset;
+
+    Vector3 mousePos;
+    Vector2 mousePos2D;
+    RaycastHit2D hit;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,14 +26,34 @@ public class ObjectSelector : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
-            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
             if (hit.collider != null)
             {
                selectedTile=hit.collider.gameObject;
             }
         }
+
+
+        //checker
+
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+        hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+        if (hit.collider != null)
+        {
+
+            if (hit.collider.gameObject.GetComponent<BaseObject>() != null)
+            {
+                //if(check color)
+                checkingObject.ChangeColliderColor();
+            }
+        }
     }
+
+
+
 }
